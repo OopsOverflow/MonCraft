99520be275690ddf94f76313667e98cc15e2ec94
Mathis <mathis.brossier@universite-paris-saclay.fr>
2021-04-23 15:37:54 +0200

    added texture atlas mipmaping

--- OpenGL/OpenGL/src/util/Loader.cpp
+++ OpenGL/OpenGL/src/util/Loader.cpp
@@ -2,6 +2,7 @@
 
 #include <SDL2/SDL_image.h>
 #include <iostream>
+#include <stdexcept>
 
 Loader::~Loader()
 {
@@ -37,36 +38,61 @@ Loader::~Loader()
 //     return std::make_unique<Mesh>(id, positions.size() / 3, vert, txr);
 // }
 
-GLuint Loader::loadTexture(const std::string& fileName)
-{
-    if(!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG))
-    {
-        std::cout << "Could not load SDL2_image with PNG files\n";
-        return EXIT_FAILURE;
+GLuint Loader::loadTexture(const std::string& fileName) {
+    if(!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+      throw std::runtime_error("could not load SDL2_image with PNG files");
     }
 
-    // Load the file(png)
     std::string path = "data/img/" + fileName + ".png";
-    SDL_Surface * img = IMG_Load(path.c_str());
-    //Convert to an RGBA8888 surface
+    SDL_Surface* img = IMG_Load(path.c_str());
+
+    if(!img) {
+      throw std::runtime_error("image file not found: " + path);
+    }
+
     SDL_Surface* rgbImg = SDL_ConvertSurfaceFormat(img, SDL_PIXELFORMAT_RGBA32, 0);
-    //Delete the old surface
     SDL_FreeSurface(img);
+
+    std::vector<SDL_Surface*> mipmaps;
+    for(int i = 1; img; i++) {
+      std::string path = "data/img/" + fileName + std::to_string(i) + ".png";
+      img = IMG_Load(path.c_str());
+      if(img) {
+        SDL_Surface* surf = SDL_ConvertSurfaceFormat(img, SDL_PIXELFORMAT_RGBA32, 0);
+        SDL_FreeSurface(img);
+        if(surf) mipmaps.push_back(surf);
+      }
+    }
 
     GLuint textureID;
     glGenTextures(1, &textureID);
     textureList.push_back(textureID);
     glBindTexture(GL_TEXTURE_2D, textureID);
     {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, rgbImg->w, rgbImg->h, 0,
-            GL_RGBA, GL_UNSIGNED_BYTE, (GLvoid *)rgbImg->pixels);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, rgbImg->w, rgbImg->h, 0,
+            GL_RGBA, GL_UNSIGNED_BYTE, (GLvoid *)rgbImg->pixels);
+        SDL_FreeSurface(rgbImg); //Delete the surface at the end of the program
+
+        if(mipmaps.empty()) {
+            glGenerateMipmap(GL_TEXTURE_2D);
+        }
+
+        else {
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, mipmaps.size());
+            for(size_t i = 0; i < mipmaps.size(); i++) {
+                SDL_Surface* surf = mipmaps.at(i);
+                glTexImage2D(GL_TEXTURE_2D, i+1, GL_RGBA, surf->w, surf->h, 0,
+                    GL_RGBA, GL_UNSIGNED_BYTE, (GLvoid *)surf->pixels);
+                SDL_FreeSurface(surf);
+            }
+        }
     }
-    SDL_FreeSurface(rgbImg); //Delete the surface at the end of the program
 
     return textureID;
 }
