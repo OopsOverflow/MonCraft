20815f7923f20087e52046e1cba489f604759bf4
Pierre Surer <pierre.surer@u-psud.fr>
2021-09-19 00:28:44 +0200

    Binary files (wip)

--- OpenGL/OpenGL/src/save/SaveManager.cpp
+++ OpenGL/OpenGL/src/save/SaveManager.cpp
@@ -20,38 +20,29 @@ SaveManager::SaveManager(std::string save_path) {
 
 std::unique_ptr<Chunk> SaveManager::getChunk(glm::ivec3 chunkPos) {
 	std::string filePath = save_path + "/chunk_" + std::to_string(chunkPos.x) + "_" + std::to_string(chunkPos.y) + "_" + std::to_string(chunkPos.z) + ".chunk";
-	std::ifstream openedFile(filePath);
-	if (!openedFile.is_open()) {
+	std::ifstream openedFile(filePath, std::fstream::binary);
+	if (!openedFile.is_open()) 
 		return std::unique_ptr<Chunk>(nullptr);
 
-	}
+	////MonCraft version
+	//char versionBuffer[3];
+	//openedFile.read(versionBuffer, 3);
 
-	std::string readedLine;
-	std::stringstream iss;
-	int extractedValue;
+	//Chunk size
+	char chunkSizeBuffer;
+	openedFile.read(&chunkSizeBuffer, 1);
 
-	getline(openedFile, readedLine);	//Version management
-
-	getline(openedFile, readedLine);	//Chunk dimensions x y z
-	iss.str(readedLine);
-	iss >> extractedValue;
-	Chunk* newChunk = new Chunk(chunkPos, extractedValue);
-
-	getline(openedFile, readedLine);	//Number of lines
+	Chunk* newChunk = new Chunk(chunkPos, chunkSizeBuffer);
 
 	int offset = 0;
-	while (getline(openedFile, readedLine)) //Bloc data
+	char buffer[3];
+	while (openedFile.read(buffer, 3)) //Block data
 	{
-		std::stringstream ss;
-		ss.str(readedLine);
-		ss >> extractedValue;
-		int blockCount = extractedValue;
-		ss >> extractedValue;
-		
-		BlockType block = (BlockType)extractedValue;
+		int blockCount = buffer[0] * 256 + buffer[1];
+		BlockType block = (BlockType)buffer[2];
 		for (int i = 0; i < blockCount; i += 1) {
-			int z = (i + offset) / (newChunk->size.y * newChunk->size.x);
-			int y = ((i + offset) / newChunk->size.x) % newChunk->size.y;
+			int y = (i + offset) / (newChunk->size.x * newChunk->size.x);
+			int z = ((i + offset) / newChunk->size.x) % newChunk->size.x;
 			int x = (i + offset) % newChunk->size.x;
 			newChunk->setBlock(glm::ivec3(x, y, z), AllBlocks::create_static(block));
 		}
@@ -69,31 +60,48 @@ std::unique_ptr<Chunk> SaveManager::getChunk(glm::ivec3 chunkPos) {
 bool SaveManager::saveChunk(std::shared_ptr<Chunk> chunk) {
 
 	std::string filePath = save_path + "/chunk_" + std::to_string(chunk->chunkPos.x) + "_" + std::to_string(chunk->chunkPos.y) + "_" + std::to_string(chunk->chunkPos.z) + ".chunk";
-	std::ofstream openedFile(filePath, std::fstream::trunc);
+	std::ofstream openedFile(filePath, std::fstream::trunc | std::fstream::binary);
 	if (!openedFile) return 0;
 
-	openedFile << "MonCraft v1.0.2" << std::endl;
-	int chunkSaveLenght = chunk->size.x * chunk->size.y * chunk->size.z;
-	openedFile << chunk->size.x << " " << chunk->size.y << " " << chunk->size.z << std::endl;
-	openedFile << chunkSaveLenght << std::endl;
+	////MonCraft version
+	//char versionBuffer[3];
+	//versionBuffer[0] = 1;
+	//versionBuffer[1] = 0;
+	//versionBuffer[2] = 2;
+	//openedFile.write(versionBuffer, 3);
+
+	//Chunk size
+	char chunkSizeBuffer = chunk->size.x;
+	openedFile.write(&chunkSizeBuffer, 1);
+
+	int chunkSaveLength = chunk->size.x * chunk->size.y * chunk->size.z;
 	int continuous = 1;
 	BlockType last = chunk->getBlock(glm::vec3(0))->type;
-	for (int i = 1; i < chunkSaveLenght; i += 1) {
-		int z = i / (chunk->size.y * chunk->size.x);
-		int y = (i / chunk->size.x) % chunk->size.y;
+	for (int i = 1; i < chunkSaveLength; i += 1) {
+		int y = i / (chunk->size.x * chunk->size.x);
+		int z = (i / chunk->size.x) % chunk->size.x;
 		int x = i % chunk->size.x;
 
 		if (chunk->getBlock(glm::vec3(x, y, z))->type == last) {
 			continuous++;
 		}
 		else {
-			openedFile << continuous << " " << (int)last << std::endl;
+			char buffer[3];
+			buffer[0] = continuous / 256;
+			buffer[1] = continuous % 256;
+			buffer[2] = (char)last;
+			openedFile.write(buffer, 3);
 			continuous = 1;
 			last = chunk->getBlock(glm::vec3(x, y, z))->type;
 		}
 
 	}
-	openedFile << continuous << " " << (int)last << std::endl;
+	char buffer[3];
+	buffer[0] = continuous / 256;
+	buffer[1] = continuous % 256;
+	buffer[2] = (char)last;
+	openedFile.write(buffer, 3);
+
 	openedFile.close();
 	return 1;
 }
