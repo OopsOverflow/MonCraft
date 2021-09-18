96f6f78f82e077bcca1852707f5693ee153c63aa
mathis <mathis.brossier@universite-paris-saclay.fr>
2021-09-18 21:30:25 +0200

    updated normal maps

--- OpenGL/OpenGL/src/gl/ShadowMap.cpp
+++ OpenGL/OpenGL/src/gl/ShadowMap.cpp
@@ -18,8 +18,12 @@ ShadowMap::ShadowMap(int size)
     glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, size, size, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    #ifndef EMSCRIPTEN
+    float borderColor[] = { 1.0f, 1.0f, 1.0f, 1.0f }; // Uncalculated shadows are white
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+    #endif
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
     glBindTexture(GL_TEXTURE_2D, 0);
   }
 
