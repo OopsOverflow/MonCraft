079ed305cee8c45f526dead5eb963186766a4364
mathis <mathis.brossier@universite-paris-saclay.fr>
2021-10-21 19:58:21 +0200

    generalized button

--- OpenGL/OpenGL/src/ui/Button.hpp
+++ OpenGL/OpenGL/src/ui/Button.hpp
@@ -11,11 +11,12 @@ namespace ui {
 class Button : public Component {
 
 protected:
-  Button(std::unique_ptr<Component> comp, std::string text, std::shared_ptr<const Font> font);
+  Button();
 
 public:
   static std::unique_ptr<Button> createPaneButton(std::string text, std::shared_ptr<const Font> font);
   static std::unique_ptr<Button> createImageButton(glm::ivec2 offset, glm::ivec2 size, std::string text, std::shared_ptr<const Font> font);
+  static std::unique_ptr<Button> create();
 
   void setHoverStyle(prop_t prop);
   void setPressedStyle(prop_t prop);
@@ -27,22 +28,12 @@ public:
 
   void onclick(std::function<void()> callback);
 
-  // forward methods from ui::Text
-  void setText(std::string text);
-  std::string getText() const;
-  void setTextColor(glm::vec4 color);
-  glm::vec4 getTextColor() const;
-  void setFontSize(float fontSize);
-  float getFontSize() const;
-
 protected:
   void onMouseIn(glm::ivec2 pos) override;
   bool onMousePressed(glm::ivec2 pos) override;
   bool onMouseReleased(glm::ivec2 pos) override;
   void onMouseOut(glm::ivec2 pos) override;
 
-  std::unique_ptr<Component> mainComp;
-  std::unique_ptr<Text> textComp;
   style_t hover;
   style_t pressed;
 
