23fb806f6f6f371c25dc96ed69b818128228a54a
Pierre Surer <pierre.surer@u-psud.fr>
2021-10-21 17:57:27 +0200

    Keymenu organisation

--- OpenGL/OpenGL/src/interface/parametersMenu/KeyMenu.hpp
+++ OpenGL/OpenGL/src/interface/parametersMenu/KeyMenu.hpp
@@ -12,5 +12,19 @@ public:
 protected:
 	KeyMenu();
 
+private : 
+	std::unique_ptr<ui::Box> forward;
+	std::unique_ptr<ui::Box> backward;
+	std::unique_ptr<ui::Box> left;
+	std::unique_ptr<ui::Box> right;
+	std::unique_ptr<ui::Box> jump;
+	std::unique_ptr<ui::Box> sneak;
+	std::unique_ptr<ui::Box> sprint;
+	//std::unique_ptr<ui::Box> break_;
+	//std::unique_ptr<ui::Box> place; mathis is tired
+	//std::unique_ptr<ui::Box> select;
+	std::unique_ptr<ui::Box> changeView;
+	std::unique_ptr<ui::Box> debug;
+
 };
 
