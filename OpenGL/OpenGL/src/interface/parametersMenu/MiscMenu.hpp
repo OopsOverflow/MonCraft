7030f1c53ac3eead681895a3f96ad7ad1390fb6c
Pierre Surer <pierre.surer@u-psud.fr>
2021-10-22 17:42:13 +0200

    MiscMenu organisation

---
+++ OpenGL/OpenGL/src/interface/parametersMenu/MiscMenu.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "ui/Box.hpp"
+
+class MiscMenu : public ui::Box {
+
+public:
+	static std::unique_ptr<Box> create();
+
+	virtual void draw() override;
+
+protected:
+	MiscMenu();
+
+private:
+	std::unique_ptr<ui::Box> enableMobileVersionButton;
+};
