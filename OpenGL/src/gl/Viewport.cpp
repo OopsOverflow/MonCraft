ee012140bdaf7e6d60ce405cba2c191bf3061ce7
Pierre Surer <pierre.surer@u-psud.fr>
2022-02-19 18:45:04 +0100

    Deleted Houssem work v12

--- OpenGL/src/gl/Viewport.cpp
+++ OpenGL/src/gl/Viewport.cpp
@@ -20,6 +20,7 @@ const int Viewport::timePerFrame = 1000 / framerate;
 
 // force use of dedicated gpu on Nvidia optimus laptops / Amd laptops
 #ifdef _WIN32
+#include <windows.h>
 extern "C" {
   __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
   __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
